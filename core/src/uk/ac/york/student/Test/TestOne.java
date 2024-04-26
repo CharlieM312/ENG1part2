981347597f6c2366fc55ce320142f53c1bcb1c02
CharlieM312 <cm2339@york.ac.uk>
2024-04-26 12:42:08 +0100

    Updated build file

--- core/src/uk/ac/york/student/Test/TestOne.java
+++
@@ -1,8 +0,0 @@
-package uk.ac.york.student.Test;
-//import static org.junit.Assert.assertEquals;
-//import org.junit.Test;
-
-public class TestOne {
-
-    
-}
---
+++ tests/src/MainMenu.java
@@ -0,0 +1,18 @@
+package tests.src;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import com.badlogic.Gdx.gdx;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+public class MainMenu {
+
+    @Test
+    public void testMainMenu() {
+        
+    }
+
+    
+}
