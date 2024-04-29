8cfb733e84340b961c2ae7198ccb37e41a31a45f
CharlieM312 <cm2339@york.ac.uk>
2024-04-29 15:04:39 +0100

    Still yet to figure out unit testing

---
+++ core/src/uk/ac/york/student/tests/testOne.java
@@ -0,0 +1,12 @@
+package uk.ac.york.student.tests;
+
+import org.jetbrains.annotations.TestOnly;
+import org.junit.Test;
+
+public class testOne {
+    
+    @Test
+    public void mytest() {
+        
+    }
+}
